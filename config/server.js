450ef6194ff5c206522d682ebd05cc4ab8717fff
basquii <jetsfool365@gmail.com>
2022-10-07 18:02:48 -0500

    deploy

--- config/server.js
+++ config/server.js
@@ -5,3 +5,9 @@ module.exports = ({ env }) => ({
     keys: env.array('APP_KEYS'),
   },
 });
+
+let port = prcess.env.PORT || 1337;
+
+app.listen (port, () => {
+  console.log(`App is running at the port' ${port}`);
+});
